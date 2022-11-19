6a1939308156feb3984ffa2cae3ca5faf1e1a7ec
Pizza and Code <rubenm.acosta@gmail.com>
2022-11-18 21:42:17 -0300

    contact-form.ts modified

--- src/app/components/footer/contact-form/contact-form.component.ts
+++ src/app/components/footer/contact-form/contact-form.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
