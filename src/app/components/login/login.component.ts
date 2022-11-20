09314d19fa4a91bf83d0677f712997601ca9f2cb
Pizza and Code <rubenm.acosta@gmail.com>
2022-11-20 20:05:41 -0300

    login validators

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,7 @@ import { Person } from 'src/app/model/person.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { ServiceService } from 'src/app/services/service.service';
 import { TokenService } from 'src/app/services/token.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -22,12 +23,20 @@ export class LoginComponent implements OnInit {
   hero_img: string = '';
   person: Person = new Person('', '', '', '', '', '', '');
 
+  form: FormGroup;
+
   constructor(
     private tokenService: TokenService,
     private authService: AuthService,
     private router: Router,
-    public personService: ServiceService
-  ) {}
+    public personService: ServiceService,
+    private formBuilder: FormBuilder
+  ) {
+    this.form = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      userName: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {
     if (this.tokenService.getToken()) {
@@ -41,23 +50,46 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  onLogin(): void {
-    this.loginUser = new LoginUser(this.userName, this.password);
-    this.authService.login(this.loginUser).subscribe(
-      (data) => {
-        this.isLogged = true;
-        this.isLogginFail = false;
-        this.tokenService.setToken(data.token);
-        this.tokenService.setUserName(data.userName);
-        this.tokenService.setAuthorities(data.authorities);
-        this.router.navigate(['/home']);
-      },
-      (err) => {
-        this.isLogged = false;
-        this.isLogginFail = true;
-        this.errMsg = err.error.message;
-        console.log(this.errMsg);
-      }
-    );
+  onLogin(event: Event): void {
+    event.preventDefault;
+
+    if (this.form.valid) {
+      this.loginUser = new LoginUser(this.userName, this.password);
+      this.authService.login(this.loginUser).subscribe(
+        (data) => {
+          this.isLogged = true;
+          this.isLogginFail = false;
+          this.tokenService.setToken(data.token);
+          this.tokenService.setUserName(data.userName);
+          this.tokenService.setAuthorities(data.authorities);
+          this.router.navigate(['/home']);
+          alert('Los datos son correctos, Bienvenido!');
+        },
+        (err) => {
+          this.isLogged = false;
+          this.isLogginFail = true;
+          this.errMsg = err.error.message;
+          console.log(this.errMsg);
+          alert('Ha ingresado datos invalidos!');
+        }
+      );
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+  get Password() {
+    return this.form.get('password');
+  }
+
+  get UserName() {
+    return this.form.get('userName');
+  }
+
+  get PasswordValid() {
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get UserNameValid() {
+    return false;
   }
 }
